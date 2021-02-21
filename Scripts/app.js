a06042989146905232759fa2a9f6c4c0668d0a3d
Kritsanasak Poungchan <p.kritsanasak@gmail.com>
2021-02-21 15:47:13 +0700

    added bootstrap and font-awesome

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,21 @@
 // IIFE Immedial Invoked Function Expression
+// anonymous selft-executing function
 (function(){
   function start(){
     console.log("App Started...");
+    let firstHeading = document.getElementById('firstHeading');
+    firstHeading.textContent = "Goodbye Cruel World!";
+     // document.getElementById('firstHeading').textContent = "Goodbye world!";
+     firstHeading.style.fontWeight = "bold";
+     let main = document.getElementsByTagName("main")[0];
+    //  console.log(main);
+     // configure new element
+     let newH2 = document.createElement("h2");
+     newH2.setAttribute("class", "display-5");
+     newH2.textContent = "Seconday Heading";
+
+     main.appendChild(newH2);
   }
   window.addEventListener("load", start);
+  // window.addEventListener('scroll',start);
 })();
